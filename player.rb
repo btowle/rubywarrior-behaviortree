991a8370dd7f2887626bf7c38a69c78b07ce3854
btowle <thebriantowle@gmail.com>
2014-04-10 11:10:53 -0700

    added rest_tree actions so warrior rests until he can survive sludge fight, beats level 3

--- player.rb
+++ player.rb
@@ -2,29 +2,57 @@ load(File.dirname(__FILE__)+"/behaviortree.rb")
 
 class Player
   def initialize
+    @sludge_damage = 6
+
     @behavior = BehaviorTree::Priority.new()
     
-    attack = BehaviorTree::Sequencer.new()
-    attack.add_child! BehaviorTree::Condition.new(->(warrior){
+    @behavior.add_child! attack_tree
+    @behavior.add_child! rest_tree
+    @behavior.add_child! walk_tree
+  end
+
+  def play_turn(warrior)
+    @behavior.run(warrior)
+  end
+
+  def rest_tree
+    rest_tree = BehaviorTree::Sequencer.new()
+    rest_tree.add_child! BehaviorTree::Condition.new(->(warrior){
+      return :failure if warrior.health > @sludge_damage
+
+      :success
+    })
+    rest_tree.add_child! BehaviorTree::Action.new(->(warrior){
+      warrior.rest!
+
+      :success
+    })
+
+    rest_tree
+  end
+
+  def attack_tree
+    attack_tree = BehaviorTree::Sequencer.new()
+    attack_tree.add_child! BehaviorTree::Condition.new(->(warrior){
       return :failure if warrior.feel.empty?
 
       :success
     })
-    attack.add_child! BehaviorTree::Action.new(->(warrior){
+    attack_tree.add_child! BehaviorTree::Action.new(->(warrior){
       warrior.attack!
       
       :success
     })
-    @behavior.add_child! attack
 
+    attack_tree
+  end
+
+  def walk_tree
     @behavior.add_child! BehaviorTree::Action.new(->(warrior){
       warrior.walk!
       
       :success
     })
-  end
 
-  def play_turn(warrior)
-    @behavior.run(warrior)
   end
 end
